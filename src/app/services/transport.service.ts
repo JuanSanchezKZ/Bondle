308f0caa6ea6e994a8dd813f59067d7041e50255
JuanSanchezKZ <juansanchez.avila.kz@gmail.com>
2026-04-05 21:10:06 -0300

    second commit

--- src/app/services/transport.service.ts
+++ src/app/services/transport.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { apiUrl } from '../../enviroment.prod';
 
 export interface DailyChallenge {
   challengeId: number;
@@ -10,23 +11,23 @@ export interface DailyChallenge {
 
 @Injectable({ providedIn: 'root' })
 export class TransportService {
-  private apiUrl = 'https://localhost:7253/api/challenge'; // Ajustá tu puerto
+
 
   constructor(private http: HttpClient) {}
 
   getDaily(): Observable<DailyChallenge> {
-    return this.http.get<DailyChallenge>(`${this.apiUrl}/daily`);
+    return this.http.get<DailyChallenge>(`${apiUrl}/daily`);
   }
 
   getAutocomplete(query: string): Observable<string[]> {
-    return this.http.get<string[]>(`${this.apiUrl}/autocomplete?query=${query}`);
+    return this.http.get<string[]>(`${apiUrl}/autocomplete?query=${query}`);
   }
 
   checkGuess(challengeId: number, guess: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/guess`, { challengeId, guess });
+    return this.http.post(`${apiUrl}/guess`, { challengeId, guess });
   }
   
   reveal(challengeId: number): Observable<any> {
-  return this.http.get(`${this.apiUrl}/reveal/${challengeId}`);
+  return this.http.get(`${apiUrl}/reveal/${challengeId}`);
 }
 }
